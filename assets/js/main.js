72f1e315538d10a3b90a19520eadeadde7449fb7
김건주 <49966040+kgj9794@users.noreply.github.com>
2024-02-10 22:51:23 +0900

    첫 업로드

---
+++ assets/js/main.js
@@ -0,0 +1,491 @@
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body'),
+		$wrapper = $('#wrapper'),
+		$header = $('#header'),
+		$footer = $('#footer'),
+		$main = $('#main'),
+		$main_articles = $main.children('article');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Fix: Flexbox min-height bug on IE.
+		if (browser.name == 'ie') {
+
+			var flexboxFixTimeoutId;
+
+			$window.on('resize.flexbox-fix', function() {
+
+				clearTimeout(flexboxFixTimeoutId);
+
+				flexboxFixTimeoutId = setTimeout(function() {
+
+					if ($wrapper.prop('scrollHeight') > $window.height())
+						$wrapper.css('height', 'auto');
+					else
+						$wrapper.css('height', '100vh');
+
+				}, 250);
+
+			}).triggerHandler('resize.flexbox-fix');
+
+		}
+
+	// Nav.
+		var $nav = $header.children('nav'),
+			$nav_li = $nav.find('li');
+
+		// Add "middle" alignment classes if we're dealing with an even number of items.
+			if ($nav_li.length % 2 == 0) {
+
+				$nav.addClass('use-middle');
+				$nav_li.eq( ($nav_li.length / 2) ).addClass('is-middle');
+
+			}
+
+	// Main.
+		var	delay = 325,
+			locked = false;
+
+		// Methods.
+			$main._show = function(id, initial) {
+
+				var $article = $main_articles.filter('#' + id);
+
+				// No such article? Bail.
+					if ($article.length == 0)
+						return;
+
+				// Handle lock.
+
+					// Already locked? Speed through "show" steps w/o delays.
+						if (locked || (typeof initial != 'undefined' && initial === true)) {
+
+							// Mark as switching.
+								$body.addClass('is-switching');
+
+							// Mark as visible.
+								$body.addClass('is-article-visible');
+
+							// Deactivate all articles (just in case one's already active).
+								$main_articles.removeClass('active');
+
+							// Hide header, footer.
+								$header.hide();
+								$footer.hide();
+
+							// Show main, article.
+								$main.show();
+								$article.show();
+
+							// Activate article.
+								$article.addClass('active');
+
+							// Unlock.
+								locked = false;
+
+							// Unmark as switching.
+								setTimeout(function() {
+									$body.removeClass('is-switching');
+								}, (initial ? 1000 : 0));
+
+							return;
+
+						}
+
+					// Lock.
+						locked = true;
+
+				// Article already visible? Just swap articles.
+					if ($body.hasClass('is-article-visible')) {
+
+						// Deactivate current article.
+							var $currentArticle = $main_articles.filter('.active');
+
+							$currentArticle.removeClass('active');
+
+						// Show article.
+							setTimeout(function() {
+
+								// Hide current article.
+									$currentArticle.hide();
+
+								// Show article.
+									$article.show();
+
+								// Activate article.
+									setTimeout(function() {
+
+										$article.addClass('active');
+
+										// Window stuff.
+											$window
+												.scrollTop(0)
+												.triggerHandler('resize.flexbox-fix');
+
+										// Unlock.
+											setTimeout(function() {
+												locked = false;
+											}, delay);
+
+									}, 25);
+
+							}, delay);
+
+					}
+
+				// Otherwise, handle as normal.
+					else {
+
+						// Mark as visible.
+							$body
+								.addClass('is-article-visible');
+
+						// Show article.
+							setTimeout(function() {
+
+								// Hide header, footer.
+									$header.hide();
+									$footer.hide();
+
+								// Show main, article.
+									$main.show();
+									$article.show();
+
+								// Activate article.
+									setTimeout(function() {
+
+										$article.addClass('active');
+
+										// Window stuff.
+											$window
+												.scrollTop(0)
+												.triggerHandler('resize.flexbox-fix');
+
+										// Unlock.
+											setTimeout(function() {
+												locked = false;
+											}, delay);
+
+									}, 25);
+
+							}, delay);
+
+					}
+
+			};
+
+			$main._hide = function(addState) {
+
+				var $article = $main_articles.filter('.active');
+
+				// Article not visible? Bail.
+					if (!$body.hasClass('is-article-visible'))
+						return;
+
+				// Add state?
+					if (typeof addState != 'undefined'
+					&&	addState === true)
+						history.pushState(null, null, '#');
+
+				// Handle lock.
+
+					// Already locked? Speed through "hide" steps w/o delays.
+						if (locked) {
+
+							// Mark as switching.
+								$body.addClass('is-switching');
+
+							// Deactivate article.
+								$article.removeClass('active');
+
+							// Hide article, main.
+								$article.hide();
+								$main.hide();
+
+							// Show footer, header.
+								$footer.show();
+								$header.show();
+
+							// Unmark as visible.
+								$body.removeClass('is-article-visible');
+
+							// Unlock.
+								locked = false;
+
+							// Unmark as switching.
+								$body.removeClass('is-switching');
+
+							// Window stuff.
+								$window
+									.scrollTop(0)
+									.triggerHandler('resize.flexbox-fix');
+
+							return;
+
+						}
+
+					// Lock.
+						locked = true;
+
+				// Deactivate article.
+					$article.removeClass('active');
+
+				// Hide article.
+					setTimeout(function() {
+
+						// Hide article, main.
+							$article.hide();
+							$main.hide();
+
+						// Show footer, header.
+							$footer.show();
+							$header.show();
+
+						// Unmark as visible.
+							setTimeout(function() {
+
+								$body.removeClass('is-article-visible');
+
+								// Window stuff.
+									$window
+										.scrollTop(0)
+										.triggerHandler('resize.flexbox-fix');
+
+								// Unlock.
+									setTimeout(function() {
+										locked = false;
+									}, delay);
+
+							}, 25);
+
+					}, delay);
+
+
+			};
+
+		// Articles.
+			$main_articles.each(function() {
+
+				var $this = $(this);
+
+				// Close.
+					$('<div class="close">Close</div>')
+						.appendTo($this)
+						.on('click', function() {
+							//location.hash = ''
+							window.history.back();
+						});
+
+				// Prevent clicks from inside article from bubbling.
+					$this.on('click', function(event) {
+						event.stopPropagation();
+					});
+
+			});
+
+		// Events.
+			$body.on('click', function(event) {
+
+				// Article visible? Hide.
+					if ($body.hasClass('is-article-visible'))
+						window.history.back();
+
+			});
+
+			$window.on('keyup', function(event) {
+
+				switch (event.keyCode) {
+
+					case 27:
+
+						// Article visible? Hide.
+							if ($body.hasClass('is-article-visible'))
+								$main._hide(true);
+
+						break;
+
+					default:
+						break;
+
+				}
+
+			});
+
+			$window.on('hashchange', function(event) {
+
+				// Empty hash?
+					if (location.hash == ''
+					||	location.hash == '#') {
+
+						// Prevent default.
+							event.preventDefault();
+							event.stopPropagation();
+
+						// Hide.
+							$main._hide();
+
+					}
+
+				// Otherwise, check for a matching article.
+					else if ($main_articles.filter(location.hash).length > 0) {
+
+						// Prevent default.
+							event.preventDefault();
+							event.stopPropagation();
+
+						// Show article.
+							$main._show(location.hash.substr(1));
+
+					}
+
+			});
+
+		// Scroll restoration.
+		// This prevents the page from scrolling back to the top on a hashchange.
+			if ('scrollRestoration' in history)
+				history.scrollRestoration = 'manual';
+			else {
+
+				var	oldScrollPos = 0,
+					scrollPos = 0,
+					$htmlbody = $('html,body');
+
+				$window
+					.on('scroll', function() {
+
+						oldScrollPos = scrollPos;
+						scrollPos = $htmlbody.scrollTop();
+
+					})
+					.on('hashchange', function() {
+						$window.scrollTop(oldScrollPos);
+					});
+
+			}
+
+		// Initialize.
+
+			// Hide main, articles.
+				$main.hide();
+				$main_articles.hide();
+
+			// Initial article.
+				if (location.hash != ''
+				&&	location.hash != '#')
+					$window.on('load', function() {
+						$main._show(location.hash.substr(1), true);
+					});
+
+})(jQuery);
+
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h2");
+const clockTitle2 = clockContainer.querySelector("h3");
+const clockTitle3 = clockContainer.querySelector("h4");
+
+var d = new Date();
+
+var week = new Array('일', '월', '화', '수', '목', '금', '토');
+
+function getTime(){
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  const fullyear = date.getFullYear();
+  const getmonth = date.getMonth();
+  const getdate = date.getDate();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}시 ${minutes < 10 ? `0${minutes}` : minutes}분 ${seconds < 10 ? `0${seconds}` : seconds}초`;
+  clockTitle2.innerText = `${getmonth+1}월 ${getdate}일 (${week[d.getDay()]})`;
+
+if(getdate == 2 && getmonth+1 == 1){
+  clockTitle.innerText = ``;
+  clockTitle2.innerText = ``;
+clockTitle3.innerText = `오늘은 운행하지 않아요!`;
+}
+if(getdate == 3 && getmonth+1 == 1){
+  clockTitle.innerText = ``;
+  clockTitle2.innerText = ``;
+clockTitle3.innerText = `오늘은 운행하지 않아요!`;
+}
+if(getdate == 11 && getmonth+1 == 2){
+  clockTitle.innerText = ``;
+  clockTitle2.innerText = ``;
+clockTitle3.innerText = `오늘은 운행하지 않아요!`;
+}
+if(getdate == 12 && getmonth+1 == 2){
+  clockTitle.innerText = ``;
+  clockTitle2.innerText = ``;
+clockTitle3.innerText = `오늘은 운행하지 않아요!`;
+}
+if(getdate == 13 && getmonth+1 == 2){
+  clockTitle.innerText = ``;
+  clockTitle2.innerText = ``;
+clockTitle3.innerText = `오늘은 운행하지 않아요!`;
+}
+if(getdate == 14 && getmonth+1 == 2){
+  clockTitle.innerText = ``;
+  clockTitle2.innerText = ``;
+clockTitle3.innerText = `오늘은 운행하지 않아요!`;
+}
+
+ 
+}
+
+
+
+function init(){
+  getTime();
+  setInterval(getTime, 1000);
+
+}
+
+init();
+
+function WinClose()
+
+ {
+
+   window.open('','_self').close();     
+
+}
+
+/*function btn(){
+    alert('셔틀이 아닌 통!학!버스입니다. 평일에만 운영합니다!\n( ※ 공식 선문대 통학버스예약사이트입니다. 안심하시고 이용하세요! )');
+}*/
+
+function btn(){
+   alert('셔틀이 아닌 통!학!버스입니다. 평일에만 운영합니다!\n( ※ 공식 선문대 통학버스예약사이트입니다. 안심하시고 이용하세요! )');
+}
+
+function bu(){
+    alert('천안터미널, 천안아산역에서 탈 수 있는 고속,시외버스 예매 사이트로 이동합니다.');
+}
+
+function joong(){
+    alert('12월 4일까지 셔틀이 운영하지 않습니다.\n시내버스나 콜밴을 이용해보세요.');
+}
+
+
+$(document).ready(function(){
+$(document).snowfall({deviceorientation : true, round : true, minSize: 1, maxSize:8,  flakeCount : 250});
+});
+
---
+++ weather.js
@@ -0,0 +1,144 @@
+const weather = document.querySelector(".js-weather");
+const weather2 = document.querySelector(".js-weather2");
+const weather3 = document.querySelector(".js-weather3");
+const weather4 = document.querySelector(".js-weather4");
+const API_KEY = "0849334613d7f1ea12dfb124b5b53003";
+const COORDS = 'coords';
+
+function getWeather(lat, lng){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`).then(function(response){
+return response.json();
+  }).then(function(json){
+    const temperature = json.main.temp;
+    const place = json.weather[0].description;
+    const location = json.name;
+    console.log(`현재위치: ${location} 현재날씨 : ${place}`);
+    weather.innerText=`현재기온은 [${Math.round(temperature)} ℃]입니다.`
+    weather4.innerText=`📍${location}`
+
+if (place=='clear sky'){
+  weather2.innerText=`현재날씨는 [맑음 ☀] 이며,`
+} 
+else if (place=='few clouds' || place=='overcast clouds'){
+  weather2.innerText=`현재날씨는 [구름 조금 ⛅] 이며,`
+}
+else if (place=='scattered clouds'){
+  weather2.innerText=`현재날씨는 [구름 많음 ☁] 이며,`
+}
+else if (place=='broken clouds'){
+weather2.innerText=`현재날씨는 [흐림 ☁] 이며,`
+}
+else if (place=='shower rain' || place=='light intensity shower rain' || place=='heavy intensity shower rain' || place=='ragged shower rain'){
+weather2.innerText=`현재날씨는 [소나기 🌦] 이며,`
+weather3.innerText=`오늘은 우산을 챙기는 것이 좋겠어요! 🌂`
+}
+else if (place=='light rain' || place=='moderate rain'){
+weather2.innerText=`현재날씨는 [비 🌧] 이며,`
+weather3.innerText=`오늘은 우산을 챙기는 것이 좋겠어요! 🌂`
+}
+else if (place=='freezing rain'){
+weather2.innerText=`현재날씨는 [얼어붙은 비 🌦] 이며,`
+weather3.innerText=`오늘은 우산을 챙기는 것이 좋겠어요! 🌂`
+}
+else if (place=='heavy intensity rain' || place=='very heavy rain' || place=='extreme rain' ){
+weather2.innerText=`현재날씨는: [폭우 🌧] 이며,`
+weather3.innerText=`오늘은 우산을 챙기는 것이 좋겠어요! 🌂`
+}
+else if (place.match('Drizzle') || place.match('drizzle')){
+weather2.innerText=`현재날씨는: [이슬비 🌧] 이며,`
+weather3.innerText=`오늘은 우산을 챙기는 것이 좋겠어요! 🌂`
+}
+else if (place.match('thunderstorm')){
+weather2.innerText=`현재날씨는 [뇌우 ⛈] 이며,`
+weather3.innerText=`오늘은 우산을 챙기는 것이 좋겠어요! 🌂`
+}
+else if (place=='light snow'){
+weather2.innerText=`현재날씨는 [약한 눈 🌨] 이며,`
+weather3.innerText=`⛄ 지금 밖에 눈와요! >.< ❄`
+}
+else if (place=='snow'){
+weather2.innerText=`현재날씨는 [눈 🌨] 이며,`
+weather3.innerText=`⛄ 지금 밖에 눈와요! >.< ❄`
+}else if (place=='Heavy snow'){
+weather2.innerText=`현재날씨는 [폭설 🌨] 이며,`
+weather3.innerText=`⛄ 지금 밖에 눈와요! >.< ❄`
+}else if (place=='Sleet'){
+weather2.innerText=`현재날씨는 [진눈깨비 🌨] 이며,`
+weather3.innerText=`⛄ 지금 밖에 눈와요! >.< ❄`
+}else if (place=='Light shower sleet'){
+weather2.innerText=`현재날씨는 [비 또는 진눈깨비 🌨] 이며,`
+weather3.innerText=`⛄ 지금 밖에 눈와요! >.< ❄`
+}else if (place=='Shower sleet'){
+weather2.innerText=`현재날씨는 [진눈깨비 🌨] 이며,`
+weather3.innerText=`⛄ 지금 밖에 눈와요! >.< ❄`
+}else if (place=='Light rain and snow'){
+weather2.innerText=`현재날씨는 [비 또는 눈 🌨] 이며,`
+weather3.innerText=`⛄ 지금 밖에 눈와요! >.< ❄`
+}else if (place=='Rain and snow'){
+weather2.innerText=`현재날씨는 [비 또는 눈 🌨] 이며,`
+weather3.innerText=`⛄ 지금 밖에 눈와요! >.< ❄`
+}else if (place=='Light shower snow'){
+weather2.innerText=`현재날씨는 [비 또는 눈 🌨] 이며,`
+weather3.innerText=`⛄ 지금 밖에 눈와요! >.< ❄`
+}else if (place=='Shower snow'){
+weather2.innerText=`현재날씨는 [소나기 눈 🌨] 이며,`
+weather3.innerText=`⛄ 지금 밖에 눈와요! >.< ❄`
+}else if (place=='Heavy shower snow'){
+weather2.innerText=`현재날씨는 [폭설 🌨] 이며,`
+weather3.innerText=`⛄ 지금 밖에 눈와요! >.< ❄`
+}
+else if (place=='mist' || place=='fog' || place=='Haze'){
+weather2.innerText=`현재날씨는 [안개 🌫] 이며,`
+}
+else if (place=='squalls' || place=='tornado'){
+weather2.innerText=`현재날씨는 [폭풍 🌪] 이며,`
+}
+else if (place=='sand/ dust whirls' || place=='sand' || place=='dust'){
+weather2.innerText=`현재날씨는 [미세먼지 🌫] 이며,`
+}
+
+  });
+
+}
+
+function saveCoords(coordsObj){
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+
+}
+
+function handleGeoSucces(position){
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError(){
+
+}
+
+function askForCoords(){
+navigator.geolocation.getCurrentPosition(handleGeoSucces,handleGeoError);
+}
+
+function loadCoords(){
+  const loadedcoords = localStorage.getItem(COORDS);
+  if(loadedcoords === null){
+    askForCoords();
+  }
+  else{
+    const parsedCoords = JSON.parse(loadedcoords);
+  getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init(){
+  loadCoords();
+
+}
+
+init();
